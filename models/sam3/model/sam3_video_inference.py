3334c1cf46383d063a00087efed4686fa92dc056
James Knoblauch <jamesknoblauch@users.noreply.github.com>
2026-06-09 23:40:45 -0400

    Update autocast dtype based on bf16 support

--- models/sam3/model/sam3_video_inference.py
+++ models/sam3/model/sam3_video_inference.py
@@ -795,7 +795,7 @@ class Sam3VideoInference(Sam3VideoBase):
         return inference_state
 
     @torch.inference_mode()
-    @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
+    @torch.autocast(device_type="cuda", dtype=torch.bfloat16 if torch.cuda.is_bf16_supported(including_emulation=False) else torch.float32)
     def warm_up_compilation(self):
         """
         Warm up the model by running a dummy inference to compile the model. This is
@@ -903,7 +903,7 @@ class Sam3VideoInference(Sam3VideoBase):
         )
         return frame_idx, self._postprocess_output(inference_state, out)
 
-    @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
+    @torch.autocast(device_type="cuda", dtype=torch.bfloat16 if torch.cuda.is_bf16_supported(including_emulation=False) else torch.float32)
     def forward(self, input: BatchedDatapoint, is_inference: bool = False):
         """This method is only used for benchmark eval (not used in the demo)."""
         # set the model to single GPU for benchmark evaluation (to be compatible with trainer)
